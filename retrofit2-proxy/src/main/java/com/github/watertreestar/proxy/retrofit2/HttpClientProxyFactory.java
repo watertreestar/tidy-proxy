4e540ea053e6247aa015713438a33f2c313b33b4
watertreestar <watertreestar@gmail.com>
2021-08-16 20:05:20 +0800

    add retrofit2 dep

--- retrofit2-proxy/src/main/java/com/github/watertreestar/proxy/retrofit2/HttpClientProxyFactory.java
+++ retrofit2-proxy/src/main/java/com/github/watertreestar/proxy/retrofit2/HttpClientProxyFactory.java
@@ -3,6 +3,9 @@ package com.github.watertreestar.proxy.retrofit2;
 import com.github.watertreestar.stub.StubContext;
 import com.github.watertreestar.stub.proxy.ProxyFactory;
 
+/**
+ * HttpClient代理创建
+ */
 public class HttpClientProxyFactory implements ProxyFactory {
     @Override
     public <T> T createProxy(Class<?> classType, StubContext context) {
